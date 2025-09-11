55adc53c65b0780e6bb3448b13dff10fd130b19b
Alanbonato <alanbnato@gmail.com>
2025-09-11 10:40:40 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = { = {
-  apiKey: "AIzaSyDYcgm0wfWgecIFvjSV3MF2QLdUuuCwoLU",
-  authDomain: "abfitness-af1cc.firebaseapp.com",
-  projectId: "abfitness-af1cc",
-  storageBucket: "abfitness-af1cc.firebasestorage.app",
-  messagingSenderId: "392827676437",
-  appId: "1:392827676437:web:386a433fffbeef99bdab1c"
-};
